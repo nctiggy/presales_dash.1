a663758835dafae107499c42296c1ebf2a87ca8b
Prelang Builder <builder@prelang.com>
2014-09-18 02:27:07 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,8 @@
 class CustomersController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   # GET /customers
